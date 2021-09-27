43f58fc1c64e70d424cf2933605c36cc53fbe511
SebasMontano <84334500+SebasMontano@users.noreply.github.com>
2021-09-26 22:38:31 -0500

    Persistency + Sorting Table

--- src/model/DishManager.java
+++ src/model/DishManager.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
@@ -8,6 +11,8 @@ import javafx.collections.ObservableList;
 public class DishManager
 {
 	private static DishManager instance;
+	private static FileWriter fw;
+	private static FileReader fr;
 	
 	public static DishManager getInstance()
 	{
@@ -24,11 +29,27 @@ public class DishManager
 	private DishManager()
 	{
 		dishes = FXCollections.observableArrayList();
+		try {
+			fw = new FileWriter("Doc/MenuReport.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public ObservableList<Dishes> getMenuData()
 	{
 		return dishes;
+	}
+	
+	public FileWriter getFW()
+	{
+		return fw;
+	}
+	
+	public FileReader getFR()
+	{
+		return fr;
 	}
 	
 	public void addDish(String name, double price, ArrayList<Ingredients> ingredients)
@@ -40,5 +61,17 @@ public class DishManager
 	public void removeDish(int ingIndex)
 	{
 		dishes.remove(ingIndex);
+	}
+	
+	public String dataToExport()
+	{
+		String data = "";
+		
+		for(Dishes dish : dishes)
+		{
+			data += dish.toString() + "\n";
+		}
+		
+		return data;
 	}
 }
